6ada29afa8d545bc561db0c87498f25bcee47a2e
Brandon Cantello <bcantello@gmail.com>
2020-07-07 22:04:14 -0700

    updates api get request to return specific pokemon based on id

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import React, {useEffect, useState} from 'react';
 import './App.css';
-import {getAllPokemon} from "./services/pokeApiHelper";
+import {getSelectedPokemon} from "./services/pokeApiHelper";
 
 function App() {
-  const [allPokemon, setAllPokemon] = useState(() => {
+  let pokemonId = 25;
+  const [pokemon, setPokemon] = useState(() => {
     const result = sessionStorage.getItem('allPokemon');
     return result ? JSON.parse(result) : {}
   });
-  console.log(allPokemon)
+  console.log('POKEMON',pokemon)
 
   // Get all pokemon from PokeAPI on initial page load and save to session storage
 
   useEffect(() => {
     const getPokemon = async () => {
-      await getAllPokemon().then(res => {
+      await getSelectedPokemon(pokemonId).then(res => {
         if (res.status === 200) {
-          setAllPokemon(res.data);
+          setPokemon(res.data);
           sessionStorage.setItem('allPokemon', JSON.stringify(res));
         } else {
           console.log(`Error retrieving data from PokeAPI. Response code ${res.status}`);
--- src/services/pokeApiHelper.js
+++ src/services/pokeApiHelper.js
@@ -4,9 +4,9 @@ const api = axios.create({
 	baseURL: 'https://pokeapi.co/api/v2/'
 });
 
-export const getAllPokemon = async (address) => {
+export const getSelectedPokemon = async (pokemonId) => {
 	try {
-		return await api.get('pokemon?limit=964');
+		return await api.get(`pokemon/${pokemonId}`);
 	} catch (e) {
 		return e;
 	}
